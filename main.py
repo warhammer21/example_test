4ace155f6e7367e6d251638f2a4b8807d5f0bb67
shreyakvashisht <shreyak.vashisht@napster.com>
2023-05-13 11:01:04 -0700

    commit_three

--- main.py
+++ main.py
@@ -3,16 +3,19 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-
+from secrets import password
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
-
+def test_funtion(a,b):
+    return a + b
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Shreyak')
     print('howareyou')
     print('and now tell me where you live?')
+    print('more info')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(test_funtion(4,5))
