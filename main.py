439cb64f4676894f1cd8f76712edde92c73b519d
shreyakvashisht <shreyak.vashisht@napster.com>
2023-05-13 10:49:57 -0700

    commit_two

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('Shreyak')
     print('howareyou')
+    print('and now tell me where you live?')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
